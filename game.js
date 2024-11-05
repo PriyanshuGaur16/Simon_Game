947bd503eca646d26ca26ffeb3643cb5985fbc57
PriyanshuGaur16 <166116297+PriyanshuGaur16@users.noreply.github.com>
2024-11-05 10:45:59 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,64 @@
+// alert("Hello");
+var gamePattern=[];
+var buttonColours=["red","blue","green","yellow"];
+var userClickedPattern=[];
+var level=0;                                                                                                                                                                
+function nextSequence(){
+    userClickedPattern=[];
+    var randomNumber=Math.random();
+    randomNumber*=4;
+    randomNumber=Math.floor(randomNumber);  //random number between 0 and 3
+    var randomChosenColour=buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $('#' + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playsound(randomChosenColour);
+    level+=1;
+    $('h1').text("Level " + level);
+}
+$(".btn").on("click",function(){
+    var userChosenColour=this.id;
+    userClickedPattern.push(userChosenColour);
+    // var audio =new Audio('sounds/' + userChosenColour + '.mp3');
+    // audio.play();
+    playsound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+})
+function playsound(color){
+    var audio =new Audio('sounds/' + color + '.mp3');
+    audio.play();
+}
+function animatePress(color){
+    $("." + color).addClass("pressed");
+    setTimeout(function(){
+        $("." + color).removeClass("pressed");
+    },100);
+}
+$('body').keypress(function(){
+    if(level===0){
+         $('h1').text("Level " + level);
+        nextSequence();
+    }
+    
+})
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel]===userClickedPattern[currentLevel]){
+        console.log("success");
+        if(gamePattern.length===userClickedPattern.length){
+            setTimeout(function(){
+                nextSequence()
+            },1000)
+        }
+    }
+    else{
+        var audioWr =new Audio('sounds/wrong.mp3');
+        audioWr.play();
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        },200)
+        $('h1').text("Game Over! Press any key to start");
+        level=0;
+        gamePattern=[];
+    }
+}
